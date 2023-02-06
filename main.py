c29ee280838c2ec777043e5f4302575ddc70feaa
Krzysztof Szokalski <szokalskikrzysiek@gmail.com>
2023-02-06 14:42:59 +0100

    DAY 27: API email send

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import requests
+from send_email import send_email
 
 api_key = '4875704e4053411cbe67b0a8c6984f7a'
 url = 'https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=4875704e4053411cbe67b0a8c6984f7a'
@@ -10,7 +11,13 @@ request = requests.get(url)
 content = request.json()
 
 # Access the article titles and description
+body =''
 for article in content["articles"]:
     print(article["title"])
     print(article["description"])
+    if article["title"] is not None:
+        body = body + article["title"] + "\n" + article["description"] + 2*"\n"
+
+body = body.encode("utf-8")
+send_email(message=body)
 
